c317bba4e895c88228037535cb014558546c9d14
Matthew Miner <misternuks@gmail.com>
2023-09-06 11:22:21 +0900

    add game loop and refactor win messages

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,28 +1,46 @@
-console.log("Yo what up")
 
-const rpsArray = ["rock", "paper", "scissors"]
+const rpsArray = ["rock", "paper", "scissors"];
 
-function getComputerChoice(rpsArray) {
-  return rpsArray[Math.floor(Math.random() * rpsArray.length)];
-}
 
-let playerSelection = prompt("Choose rock, paper, or scissors.").toLowerCase();
+function game() {
 
-function playRound(playerSelection, computerSelection) {
+  let playerScore = 0;
+  let computerScore = 0;
+  let round = 0;
 
-  console.log(playerSelection, computerSelection)
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    return `You win! ${playerSelection} beats ${computerSelection}!`;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return `You win! ${playerSelection} beats ${computerSelection}!`;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return `You win! ${playerSelection} beats ${computerSelection}!`;
-  } else if (playerSelection == computerSelection) {
-    return `It's a tie between ${playerSelection} and ${computerSelection}!`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  while (round < 5) {
+    function getComputerChoice(rpsArray) {
+      return rpsArray[Math.floor(Math.random() * rpsArray.length)];
+    }
+
+    const computerSelection = getComputerChoice(rpsArray);
+
+    let playerSelection = prompt("Choose rock, paper, or scissors.").toLowerCase();
+
+    const playerWinMessage = `You win! ${playerSelection} beats ${computerSelection}!`;
+
+    function playRound(playerSelection, computerSelection) {
+
+      console.log(playerSelection, computerSelection)
+      if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
+        return playerWinMessage;
+      } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        return playerWinMessage;
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        return playerWinMessage;
+      } else if (playerSelection == computerSelection) {
+        return `It's a tie between ${playerSelection} and ${computerSelection}!`;
+      } else {
+        computerScore++;
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+      }
+    }
+    round++;
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
   }
+  console.log(`The final score is Player: ${ playerScore }, Computer: ${ computerScore }.`)
 }
-
-const computerSelection = getComputerChoice(rpsArray);
-console.log(playRound(playerSelection, computerSelection));
