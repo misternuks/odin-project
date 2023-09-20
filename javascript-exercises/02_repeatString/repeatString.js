a07c5c3d1de89510a027042ef274736253166588
Matthew Miner <misternuks@gmail.com>
2023-09-21 06:58:18 +0900

    add .eslintrc for es6 support; complete exercise 02

---
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -0,0 +1,10 @@
+const repeatString = function(string, num) {
+  if(num >= 0) {
+    return string.repeat(num);
+  } else {
+    return 'ERROR'
+  }
+};
+
+// Do not edit below this line
+module.exports = repeatString;
